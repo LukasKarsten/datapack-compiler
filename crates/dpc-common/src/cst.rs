30cb73333a59f2c6450294193f0dc4b4a7dc9d30
Lukas Karsten <lukaskarsten@protonmail.com>
2025-03-24 10:05:52 +0100

    Remove unused CST visitor functions

--- crates/dpc-common/src/cst.rs
+++ crates/dpc-common/src/cst.rs
@@ -84,67 +84,41 @@ pub struct Angle {
 }
 
 pub trait Visitor: Sized {
-    fn visit_item(&mut self, item: &Item) {
-        walk_item(self, item);
-    }
     fn visit_comment(&mut self, _comment: &Span) {}
-    fn visit_command(&mut self, command: &Command) {
-        walk_command(self, command);
-    }
     fn visit_argument(&mut self, argument: &Argument) {
         walk_argument(self, argument);
     }
-    fn visit_block(&mut self, block: &Block) {
-        walk_block(self, block);
-    }
-    fn visit_boolean(&mut self, _boolean: &Boolean) {}
-    fn visit_integer(&mut self, _integer: &Integer) {}
-    fn visit_float(&mut self, _float: &Float) {}
-    fn visit_double(&mut self, _double: &Double) {}
-    fn visit_string(&mut self, _string: &String) {}
-    fn visit_angle(&mut self, _angle: &Angle) {}
     fn visit_parse_error(&mut self, _error: &ParseError) {}
 }
 
 pub fn walk_item(visitor: &mut impl Visitor, item: &Item) {
     match item {
-        Item::Command(command) => visitor.visit_command(command),
+        Item::Command(command) => walk_command(visitor, command),
         Item::Comment(comment) => visitor.visit_comment(comment),
     }
 }
 
 pub fn walk_command(visitor: &mut impl Visitor, command: &Command) {
-    for argument in &command.args {
-        visitor.visit_argument(argument);
-    }
     if let Some(error) = &command.error {
         visitor.visit_parse_error(error);
+    }
+    for argument in &command.args {
+        visitor.visit_argument(argument);
     }
 }
 
 pub fn walk_argument(visitor: &mut impl Visitor, argument: &Argument) {
-    match &argument.value {
-        ArgumentValue::Literal => (),
-        ArgumentValue::Block(block) => visitor.visit_block(block),
-        ArgumentValue::Boolean(boolean) => visitor.visit_boolean(boolean),
-        ArgumentValue::Integer(integer) => visitor.visit_integer(integer),
-        ArgumentValue::Float(float) => visitor.visit_float(float),
-        ArgumentValue::Double(double) => visitor.visit_double(double),
-        ArgumentValue::String(string) => visitor.visit_string(string),
-        ArgumentValue::Angle(string) => visitor.visit_angle(string),
-    }
-
     for error in &argument.errors {
         visitor.visit_parse_error(error);
+    }
+
+    if let ArgumentValue::Block(block) = &argument.value {
+        walk_block(visitor, block);
     }
 }
 
 pub fn walk_block(visitor: &mut impl Visitor, block: &Block) {
     for item in &block.items {
-        visitor.visit_item(item);
+        walk_item(visitor, item);
     }
-}
-
-pub fn walk_angle(visitor: &mut impl Visitor, angle: &Angle) {
-    visitor.visit_float(&angle.value);
 }
